263f44daa0cd9cb362118be753ebe3644a8240dc
KNS121 <komaroff.ns@gmail.com>
2025-05-06 13:07:05 +0300

    repeat_seq_function

--- functions_for_decrypt.py
+++ functions_for_decrypt.py
@@ -10,3 +10,7 @@ def find_repeated_sequences(text, min_length=2):
         sequences[seq].append(i)
 
     return {seq: pos for seq, pos in sequences.items() if len(pos) > 1}
+
+def distance_between_repeat(rep_seq):
+    distances = []
+
